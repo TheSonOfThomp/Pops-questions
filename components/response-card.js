f8341b24c11a2e1fb184ca95d96c6bd11ec5d07c
Adam Thompson <adam.m.thompson@icloud.com>
2022-01-15 15:55:29 -0500

    Updates responses page

---
+++ components/response-card.js
@@ -0,0 +1,45 @@
+import { formatDate } from '../utils'
+import styles from '../styles/Home.module.scss'
+import cardStyles from './response-card.module.scss'
+import { useState } from 'react'
+import Link from 'next/link'
+
+
+const collapsedCharLimit = 128;
+
+export const ResponseCard = ({
+  date,
+  question,
+  questionId,
+  answer,
+  answerId,
+}) => {
+
+  const [isExpanded, setExpanded] = useState(false)
+
+  const content = isExpanded
+    ? answer.split('\n').map(line => <p>{line}</p>)
+    : (answer.slice(0, collapsedCharLimit) + '...').split('\n').map(line => <p>{line}</p>)
+
+  return (
+    <div className={styles.response}>
+      <span>{formatDate(date)}</span>
+      <h2>{question[0]}</h2>
+      <div className={cardStyles.content}>
+        {content}
+      </div>
+      <div className={cardStyles.button_wrapper}>
+        <button
+          className={`${styles.button} ${styles.button_light} ${styles.button_small}`}
+          onClick={() => setExpanded(!isExpanded)}
+        >Show {isExpanded ? 'less' : 'full answer'}</button>
+        
+        <Link href={`/edit/${answerId}`}>
+          <span className={`${styles.button} ${styles.button_small}`}>
+            Edit response
+          </span>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- pages/responses.js
+++ pages/responses.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { fetcher, formatDate } from '../utils'
 import styles from '../styles/Home.module.scss'
 import useSWR from 'swr'
+import { ResponseCard } from '../components/response-card'
 
 const title = "Pop's Responses"
 const responsesEndpoint = "/.netlify/functions/responses/"
@@ -26,23 +27,27 @@ export default function Home() {
       </Link>
 
       {
-        responses && responses.map(response => {
+        doResponsesExist && responses.map(response => {
+
           const {
+            id: answerId,
             fields: {
               Answer: answer,
               'Answered on': date,
-              'Question Text': question
+              'Question Text': question,
+              'Question': questionId,
             }
           } = response
           
           return question && (
-            <div className={styles.response}>
-              <span>{formatDate(date)}</span>
-              <h2>{question[0]}</h2>
-              <div>
-                {answer.split('\n').map(line => <p>{line}</p>)}
-              </div>
-            </div>
+            <ResponseCard
+              key={questionId}
+              date={date}
+              question={question}
+              questionId={questionId}
+              answer={answer}
+              answerId={answerId}
+            />
           )
         })
       }
